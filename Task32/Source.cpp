083e176d9f8431af63db469ef0c8a0f106675fe1
Anna <morganas.scream@gmail.com>
2017-12-22 04:07:44 +0200

    upd

--- Task32/Source.cpp
+++ Task32/Source.cpp
@@ -1,3 +1,7 @@
+//Given two lines a and b, output a string x of maximum length, consisting of,
+//letters such that there exists a permutation x that is a substring
+//permutation a and at the same time being a permutation substring b.
+
 #include "FindReshuffle.h"
 #include <iostream>
 
